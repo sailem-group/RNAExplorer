f0df0417afa395a6be98e7b4be52da7f2a557b2d
shivprasadkcl <shivprasad.jamdade@kcl.ac.uk>
2026-02-01 12:05:58 +0530

    Change or the URL

--- core/views.py
+++ core/views.py
@@ -2,6 +2,7 @@
 from django.conf import settings
 from django.shortcuts import render, redirect
 from django.views.decorators.http import require_http_methods
+from django.urls import reverse
 from .middleware import DEMO_SESSION_KEY
 
 @require_http_methods(["GET", "POST"])
@@ -12,6 +13,6 @@ def demo_access(request):
         expected = getattr(settings, "DEMO_ACCESS_CODE", "")
         if expected and code == expected:
             request.session[DEMO_SESSION_KEY] = True
-            return redirect("/")
+            return redirect(reverse("home"))
         error = "Invalid code"
     return render(request, "demo_access.html", {"error": error})
